a550e03aa676f7e66524267550ff03f19ba30762
unknown <wafiwafiwafi90@gmail.com>
2024-05-23 05:00:19 -0700

    y

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,20 @@ import type { Metadata } from "next";
 import { Inter as FontSans } from "next/font/google"
 import "./globals.css";
 import Header from "@/components/header";
-;
+import data from "@/data.json"
+  ;
 
 const fontSans = FontSans({
   subsets: ["latin"],
   variable: "--font-sans",
 })
 export const metadata: Metadata = {
-  title: "gatau",
-  description: "gatau ahh bingung",
+  title: "Zilog",
+  description: "This is My Frist Website using SEO",
   icons: {
     icon: "/vercel.svg"
   },
-  keywords: ["macan tutul", "macan", "harimau", "harimau sumatra", "macan sumatara", "macan", "harimau sumatra"],
+  keywords: data.data,
   verification: {
     google: "zjK6Hxhrx6PtXe4xZsokA0VHOV6zsNiBnQ45jAKwot8"
   }
@@ -28,7 +29,7 @@ export default function RootLayout({
   return (
     // <StoreProvider>
     <html lang="en">
-      <meta name="google-site-verification" content="zjK6Hxhrx6PtXe4xZsokA0VHOV6zsNiBnQ45jAKwot8" />
+      {/* <meta name="google-site-verification" content="zjK6Hxhrx6PtXe4xZsokA0VHOV6zsNiBnQ45jAKwot8" /> */}
       <body >
         <Header />
         {children}</body>
